7724bc44d6f9f8c34e0ccc9af950c916a6836ed3
Elijah Logan <elijah.logan@purchase.edu>
2019-04-18 11:21:39 -0400

    full app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Business from './components/Business/Business';
+import Yelp from './components/util/Yelp'
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {businesses:[]}
+    this.searchYelp = this.searchYelp.bind(this)
+  }
+
+searchYelp(term,location,sortBy){
+  Yelp.search(term,location,sortBy).then(businesses=> {
+    this.setState({businesses:businesses})
+  })
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SearchBar searchYelp = {this.searchYelp}/>
+        <BusinessList businesses = {this.state.businesses}/>
       </div>
+
     );
   }
 }
